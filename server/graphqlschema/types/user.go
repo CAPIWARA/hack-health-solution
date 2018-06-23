2a4af1eaafcc51d2b84663bc738c67eca9dce0c7
Marcos Vinicius Cardoso <ruizde169@gmail.com>
2018-06-23 20:13:08 -0300

    add users attributes

--- server/graphqlschema/types/user.go
+++ server/graphqlschema/types/user.go
@@ -7,10 +7,43 @@ var User = graphql.NewObject(graphql.ObjectConfig{
 	Fields: graphql.Fields{
 		"name": &graphql.Field{
 			Type:        graphql.String,
-			Description: "user name",
+			Description: "name",
 		},
 		"email": &graphql.Field{
 			Type:        graphql.String,
-			Description: "user email",
+			Description: "email",
 		},
+		"birthday": &graphql.Field{
+			Type:        graphql.String,
+			Description: "birthday",
+		},
+		"points": &graphql.Field{
+			Type:        graphql.String,
+			Description: "points",
+		},
+		"socialName": &graphql.Field{
+			Type:        graphql.String,
+			Description: "socialName",
+		},
+		"gender": &graphql.Field{
+			Type:        graphql.String,
+			Description: "gender",
+		},
+		"orientation": &graphql.Field{
+			Type:        graphql.String,
+			Description: "orientation",
+		},
+		"ethnicity": &graphql.Field{
+			Type:        graphql.String,
+			Description: "ethnicity",
+		},
+		"genderIdentity": &graphql.Field{
+			Type:        graphql.String,
+			Description: "genderIdentity",
+		},
+		"image": &graphql.Field{
+			Type:        graphql.String,
+			Description: "image",
+		},
+
 	}})
--- server/users/user.go
+++ server/users/user.go
@@ -8,10 +8,17 @@ import (
 )
 
 type User struct {
-	Id         string `json:"-" bson:"_id"`
-	Name       string `json:"name"`
-	Points     int    `json:"points"`
-	SocialName string `json:"socialname,omitempty"`
+	Id             string `json:"-" bson:"_id"`
+	Name           string `json:"name"`
+	Email          string `json:"email"`
+	Birthday       string `json:"birthday"`
+	Points         int    `json:"points"`
+	SocialName     string `json:"socialName,omitempty"`
+	Gender         string `json:"gender"`
+	Orientation    string `json:"orientation"`
+	Ethnicity      string `json:"ethnicity"`
+	GenderIdentity string `json:"genderIdentity"`
+	Image 		   string `json:"image"`
 }
 
 func GetUser(id string) (*User, error) {
